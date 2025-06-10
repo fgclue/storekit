2a7a3b123cacebf8e43b0ef747c2ee25a914d1b8
fgclue <lost@biitle.nl>
2025-06-09 21:56:40 -0300

    Add build script base

---
+++ src/build.rs
@@ -0,0 +1,22 @@
+use std::process;
+
+fn main() {
+    let backend: String = match std::env::var("BACKEND") {
+        Ok(value) => value,
+        Err(_) => "null".to_string()
+    };
+
+    let bindings: String = match std::env::var("BINDS") {
+        Ok(value) => value,
+        Err(_) => "null".to_string()
+    };
+
+    if backend == "null".to_string() {
+        println!("cargo::error=No backend selected! (Use the BACKEND env variable)");
+        process::abort();
+    }
+    if bindings == "null".to_string() { println!("cargo::warning=No bindings selected! (Use the BINDS env variable)") }
+
+    println!("cargo::warning=Using src/backends/modules/{}.rs as backend", backend.to_string());
+    println!("cargo::warning=& src/backends/bindings/{}.rs as bindings", bindings.to_string());
+}
