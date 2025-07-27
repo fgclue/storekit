fa77838b0045b62a97003813b0ab1be068f35510
Clue <87581499+fgclue@users.noreply.github.com>
2025-07-27 23:31:48 +0000

    Move messages class to a separate file for better organization

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,15 @@
 mod backends;
+mod message;
 
-/// Status of the backend
-/// 
-/// - `Waiting`: Backend just started and still hasn't done anything
-/// - `Finished`: Done doing a task
-/// - `Failed(String)`: Failed doing a task
-/// - `Dead`: Backend has crashed
-/// - `Working`: Doing something non-critical such as searching packages or syncing.
-/// - `WorkingImportant`: Doing something critical such as installing, removing or cleaning.
-/// - `Updating(UpdateType)`: Updating the system, distribution or a package.
-pub enum BackendStatus {
-    Waiting,
-    Finished,
-    Failed(String),
-    Dead,
-    Working,
-    WorkingImportant,
-    Updating(UpdateType),
-}
-
-/// Types of updates
-/// 
-/// `DistUpdate` is not supported on all backends, such as pacman.
-/// 
-/// If not supported, the backend will use System instead.
-/// 
-/// Ideally you should avoid `DistUpdate`.
-pub enum UpdateType {
-    DistUpdate, // not supported on all backends, some will ignore this
-    System,
-    Package(String)
-}
-
-/// Reason for failure
-pub enum FailureReason {
-    NonexistentCommand,
-    BackendDied,
-    NonexistentPackage,
-    FailedConnection,
-    PartialUpdateNotAllowed,
-    NullBackend,
-    Other(String),
-}
-
-/// Message sent to a backend
-pub enum Message {
-    Install(String),
-    Remove(String),
-    Query(String),
-    Upgrade(UpdateType),
-    Sync(),
-    Clean(), // not supported on all backends, some will ignore this
-}
+pub use message::BackendStatus as BackendStatus;
+pub use message::FailureReason as FailureReason;
+pub use message::Message as Message;
+pub use message::UpdateType as UpdateType;
 
 /// A backend, which handles messages and sends its status back.
 pub trait Backend {
     fn send_message(message: Message) -> Result<(), FailureReason>;
     fn get_status() -> BackendStatus;
-}
-
-pub fn send_message(message: Message) {
 }
 
 #[cfg(test)]
---
+++ src/message.rs
@@ -0,0 +1,52 @@
+/// Status of the backend
+/// 
+/// - `Waiting`: Backend just started and still hasn't done anything
+/// - `Finished`: Done doing a task
+/// - `Failed(String)`: Failed doing a task
+/// - `Dead`: Backend has crashed
+/// - `Working`: Doing something non-critical such as searching packages or syncing.
+/// - `WorkingImportant`: Doing something critical such as installing, removing or cleaning.
+/// - `Updating(UpdateType)`: Updating the system, distribution or a package.
+pub enum BackendStatus {
+    Waiting,
+    Finished,
+    Failed(String),
+    Dead,
+    Working,
+    WorkingImportant,
+    Updating(UpdateType),
+}
+
+/// Types of updates
+/// 
+/// `DistUpdate` is not supported on all backends, such as pacman.
+/// 
+/// If not supported, the backend will use System instead.
+/// 
+/// Ideally you should avoid `DistUpdate`.
+pub enum UpdateType {
+    DistUpdate, // not supported on all backends, some will ignore this
+    System,
+    Package(String)
+}
+
+/// Reason for failure
+pub enum FailureReason {
+    NonexistentCommand,
+    BackendDied,
+    NonexistentPackage,
+    FailedConnection,
+    PartialUpdateNotAllowed,
+    NullBackend,
+    Other(String),
+}
+
+/// Message sent to a backend
+pub enum Message {
+    Install(String),
+    Remove(String),
+    Query(String),
+    Upgrade(UpdateType),
+    Sync(),
+    Clean(), // not supported on all backends, some will ignore this
+}
